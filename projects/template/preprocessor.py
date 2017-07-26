61636c9e4a6cc5f603f39ba5341f19c93bd513be
rjewing <ewing.rj@gmail.com>
2017-07-26 15:20:03 -0700

    added template project files

---
+++ projects/template/preprocessor.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""proprocessor.AbstractPreProcessor implementation for preprocessing data"""
+from preprocessor import AbstractPreProcessor
+
+
+class PreProcessor(AbstractPreProcessor):
+    def _process_data(self):
+        pass
