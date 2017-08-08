de821323a664ce1bdb7a1e5ba74d90c8b3031d55
rjewing <ewing.rj@gmail.com>
2017-08-07 19:44:17 -0700

    bug fix for rdf2csv output

--- process/rdf2csv.py
+++ process/rdf2csv.py
@@ -6,13 +6,15 @@ import subprocess
 def convert_rdf2csv(input_dir, output_dir, sparql_file, query_fetcher_path):
     logging.debug("converting reasoned data to csv for directory {}".format(input_dir))
 
-    process = subprocess.run(['java', '-jar', query_fetcher_path,
-                              '-i', input_dir,
-                              '-inputFormat', 'TURTLE',
-                              '-o', output_dir,
-                              '-numThreads', '8',
-                              '-sparql', sparql_file],
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT,
-                             check=True)
-    logging.debug(process.communicate())
+    process = subprocess.Popen(['java', '-jar', query_fetcher_path,
+                                '-i', input_dir,
+                                '-inputFormat', 'TURTLE',
+                                '-o', output_dir,
+                                '-numThreads', '8',
+                                '-sparql', sparql_file],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+
+    with process.stdout:
+        for line in iter(process.stdout.readline, b''):
+            logging.debug(line)
