26342787b1adbf01dda859c4f592e30438b5469c
rjewing <ewing.rj@gmail.com>
2017-08-07 17:04:04 -0700

    fix invalid import in rdf2csv

--- process/rdf2csv.py
+++ process/rdf2csv.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
+import subprocess
 
 
 def convert_rdf2csv(input_dir, output_dir, sparql_file, query_fetcher_path):
     logging.debug("converting reasoned data to csv for directory {}".format(input_dir))
 
-    process = os.subprocess.run(['java', '-jar', query_fetcher_path,
-                                 '-i', input_dir,
-                                 '-inputFormat', 'TURTLE',
-                                 '-o', output_dir,
-                                 '-numThreads', 8,
-                                 '-sparql', sparql_file],
-                                stdout=os.subprocess.PIPE,
-                                stderr=os.subprocess.STDOUT,
-                                check=True)
+    process = subprocess.run(['java', '-jar', query_fetcher_path,
+                              '-i', input_dir,
+                              '-inputFormat', 'TURTLE',
+                              '-o', output_dir,
+                              '-numThreads', 8,
+                              '-sparql', sparql_file],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             check=True)
     logging.debug(process.communicate())
