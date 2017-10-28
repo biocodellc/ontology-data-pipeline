f542066ed77291acd8ef6e381e0b6d979064dc07
John Deck <jdeck88@gmail.com>
2017-10-28 05:27:07 -0700

    adding logger debug output to rdf2csv

--- process/rdf2csv.py
+++ process/rdf2csv.py
@@ -6,19 +6,12 @@ import subprocess
 def convert_rdf2csv(input_dir, output_dir, sparql_file, query_fetcher_path):
     logging.debug("converting reasoned data to csv for directory {}".format(input_dir))
 
-    #print(subprocess.list2cmdline(['java', '-jar', query_fetcher_path,
-    #                '-i', input_dir,
-    #                '-inputFormat', 'TURTLE',
-    #                '-o', output_dir,
-    #                '-numThreads', '8',
-    #                '-sparql', sparql_file]))
-    subprocess.run(['java', '-jar', query_fetcher_path,
-                    '-i', input_dir,
-                    '-inputFormat', 'TURTLE',
-                    '-o', output_dir,
-                    '-numThreads', '8',
-                    '-sparql', sparql_file],
-                   check=True)
+    cmd = ['java', '-jar', query_fetcher_path, '-i', input_dir, '-inputFormat', 'TURTLE', '-o', output_dir, '-numThreads', '8', '-sparql', sparql_file] 
+
+    logging.debug("running query_fetcher with: ")
+    logging.debug(subprocess.list2cmdline(cmd))
+
+    subprocess.run(cmd, check=True)
 
     # with process.stdout:
     #     for line in iter(process.stdout.readline, b''):
