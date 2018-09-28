cd5607e06379529f936fc0fef69a4954bb4eabd0
John Deck <jdeck88@gmail.com>
2018-09-28 07:23:24 -0700

    Setting herbarium project to recognize individual plants.  Pipeline now recognizes and outputs individualID as a data attribute, fixes #24

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -27,7 +27,7 @@ class AbstractPreProcessor(object):
 
     The __input_dir is available as a private class attribute.
     """
-    headers = ['record_id', 'scientific_name', 'genus', 'specific_epithet', 'year', 'day_of_year', 'latitude',
+    headers = ['record_id', 'individualID', 'scientific_name', 'genus', 'specific_epithet', 'year', 'day_of_year', 'latitude',
                'longitude', 'source', 'sub_source', 'phenophase_name', 'basis_of_record', 'lower_count_partplant', 
                'upper_count_partplant', 'lower_count_wholeplant', 'upper_count_wholeplant', 'lower_percent_partplant', 'upper_percent_partplant', 'lower_percent_wholeplant', 'upper_percent_wholeplant']
 
